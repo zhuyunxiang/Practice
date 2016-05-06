acbdb8c0ec155eeb3f4f42d75d4a4f7a722662ef
zhuyunxiang <zhuyunxiang@snail.com>
2016-05-06 17:46:14 +0800

    添加排序和筛选

--- Angulr-Utils/angular-utils-table.js
+++ Angulr-Utils/angular-utils-table.js
@@ -9,15 +9,23 @@ UtilsTableDirectives.directive('utilTable', [function() {
     return {
         restrict: 'ECA',
         transclude: true,
-        template: '<table class="table table-bordered"><tr><th ng-repeat="titleItem in attrbutes" ng-show="attrbutes&&attrbutes.length">{{titleItem.title}}</th><th ng-show="options&&options.length">操作</th></tr><tr ng-repeat="rowData in dataSource|limitTo:limitLength"><td ng-repeat="attrInfo in rowData|filterAttributes:attrbutes">{{attrInfo}}</td><td ng-show="options&&options.length"><span ng-repeat="oItem in options" util-table-btn-options oitem="oItem" optiondata="rowData"></span></td></tr></table>',
+        template: '<table class="table table-bordered"><tr><th ng-repeat="titleItem in config.attr" ng-show="config.attr&&config.attr.length" <span ng-click="setOrderInfo(titleItem)" style="cursor:pointer">{{titleItem.title}}<i class="glyphicon" ng-show="titleItem.key==sortPredicate" ng-class="{true:b, false:a}[reverseOrder]"></i></span></th><th ng-show="config.opt&&config.opt.length">操作</th></tr><tr ng-repeat="rowData in dataSource|limitTo:config.limitLength|filter:config.searchKeyword|autoSortBy:sortPredicate:reverseOrder"><td ng-repeat="attrInfo in rowData|filterAttributes:config.attr">{{attrInfo}}</td><td ng-show="config.opt&&config.opt.length"><span ng-repeat="oItem in config.opt" util-table-btn-options oitem="oItem" optiondata="rowData"></span></td></tr></table>',
         replace: true,
         scope: {
-            attrbutes: "=attrbutes",
+            config: "=config",
             dataSource: "=datasource",
-            options: "=options",
         },
         link: function($scope, $element, $attrs) {
-            // 操作
+            $scope.sortPredicate = "name";
+            $scope.reverseOrder = false;
+
+            $scope.a = 'glyphicon-sort-by-attributes';
+            $scope.b = 'glyphicon-sort-by-attributes-alt';
+
+            $scope.setOrderInfo = function (titleItem) {
+                $scope.sortPredicate = titleItem.key;
+                $scope.reverseOrder =! $scope.reverseOrder;
+            }
         }
     };
 }]);
@@ -27,40 +35,38 @@ UtilsTableDirectives.directive('utilTableBtnOptions', [function() {
     return {
         restrict: 'ECA',
         transclude: true,
-        template: '<span><button class="btn btn-default {{oitem.class}}" style="margin-right:10px;" ng-style="oitem.style" ng-show="oitem.isBtn">{{oitem.title}}</button><select ng-show="oitem.isSelect"><option>无</option></select></span>',
+        template: '<span><button class="{{oitem.class}}"ng-style="oitem.style" ng-show="oitem.isBtn">{{oitem.title}}</button><select ng-show="oitem.isSelect" ng-model="selectModel" class="{{oitem.class}}"ng-style="oitem.style" ng-options="k.value as k.name for k in oitem.selectOptions"><option value="">无</option></select></span>',
         replace: true,
         scope: {
             oitem: "=oitem",
-            optiondata: "=optiondata"
+            optiondata: "=optiondata",
         },
         link: function($scope, $element, $attrs) {
-            console.log($scope.oitem);
-
-            if($scope.oitem.isBtn) {
+            // 按钮事件回调
+            if ($scope.oitem.isBtn) {
                 $element.bind('click', function(a) {
                     var callback = $scope.oitem.callback || function() {};
                     callback($scope.optiondata, $scope.oitem);
                 });
             }
-            if($scope.oitem.isSelect) {
+            // Select事件回调
+            if ($scope.oitem.isSelect) {
                 $element.bind('change', function(a) {
                     var callback = $scope.oitem.callback || function() {};
-                    callback($scope.optiondata, $scope.oitem);
+                    callback($scope.optiondata, $scope.selectModel, $scope.oitem);
                 });
             }
-
-
         }
     };
 }]);
 
 // 过滤数据对象中不在指示范围内的对象
 UtilsTableFilters.filter('filterAttributes', [function() {
-    return function(input, attributes) {
-        if(attributes&&attributes.length){
+    return function(input, attributes, filterOptions) {
+        // 判断哪些字段需要显示
+        if (attributes && attributes.length) {
             var item = {};
             for (var i = 0; i < attributes.length; i++) {
-                if (input[attributes[i]['key']])
                 item[attributes[i]['key']] = input[attributes[i]['key']];
             }
         } else {
@@ -68,5 +74,107 @@ UtilsTableFilters.filter('filterAttributes', [function() {
         }
 
         return item;
+    };
+}]);
+
+UtilsTableFilters.filter('autoSortBy', ['$filter', '$parse', function($filter, $parse) {
+    return function(array, sortPredicate, reverseOrder) {
+        if (!(array instanceof Array)) return array;
+        if (!sortPredicate) return array;
+        sortPredicate = sortPredicate instanceof Array ? sortPredicate : [sortPredicate];
+        sortPredicate = map(sortPredicate, function(predicate) {
+            var descending = false,
+                get = predicate || identity;
+            if (angular.isString(predicate)) {
+                if ((predicate.charAt(0) == '+' || predicate.charAt(0) == '-')) {
+                    descending = predicate.charAt(0) == '-';
+                    predicate = predicate.substring(1);
+                }
+                get = $parse(predicate);
+                if (get.constant) {
+                    var key = get();
+                    return reverseComparator(function(a, b) {
+                        return compare(a[key], b[key]);
+                    }, descending);
+                }
+            }
+            return reverseComparator(function(a, b) {
+                return compare(get(a), get(b));
+            }, descending);
+        });
+        var arrayCopy = [];
+        for (var i = 0; i < array.length; i++) {
+            arrayCopy.push(array[i]);
+        }
+        var out = arrayCopy.sort(reverseComparator(comparator, reverseOrder));
+        return out;
+
+        function comparator(o1, o2) {
+            for (var i = 0; i < sortPredicate.length; i++) {
+                var comp = sortPredicate[i](o1, o2);
+                if (comp !== 0) return comp;
+            }
+            return 0;
+        }
+
+        function reverseComparator(comp, descending) {
+            return toBoolean(descending) ? function(a, b) {
+                return comp(b, a);
+            } : comp;
+        }
+
+        function toBoolean(value) {
+            if (typeof value === 'function') {
+                value = true;
+            } else if (value && value.length !== 0) {
+                var v = angular.lowercase("" + value);
+                value = !(v == 'f' || v == '0' || v == 'false' || v == 'no' || v == 'n' || v == '[]');
+            } else {
+                value = false;
+            }
+            return value;
+        }
+
+        function map(obj, iterator, context) {
+            var results = [];
+            angular.forEach(obj, function(value, index, list) {
+                results.push(iterator.call(context, value, index, list));
+            });
+            return results;
+        }
+
+        function compare(v1, v2) {
+            var t1 = typeof v1;
+            var t2 = typeof v2;
+            if (v1 == null) {
+                return -1;
+            }
+            if (v2 == null) {
+                return 1;
+            }
+            if (t1 == t2) {
+                if (t1 == "string") {
+                    if (v1.length == 0) {
+                        return -1;
+                    }
+                    if (v2.length == 0) {
+                        return 1;
+                    }
+
+                    v1 = v1.toLowerCase();
+                    v2 = v2.toLowerCase();
+                }
+
+                if (!isNaN(Number(v1)) && !isNaN(Number(v1))) {
+                    v1 = Number(v1);
+                    v2 = Number(v2);
+                }
+                if (v1 === v2) return 0;
+                return v1 < v2 ? -1 : 1;
+            } else {
+                return t1 < t2 ? -1 : 1;
+            }
+        }
+
     };
 }]);
